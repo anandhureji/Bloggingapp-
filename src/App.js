e440caa02c355a697a67695ec5cc8e52fcb9cc4a
Reji <2111896@cognizant.com>
2022-10-20 12:24:29 +0530

    routing feature added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 
 import { useContext } from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Component/Navbar';
+import HomePage from './Pages/HomePage';
+import PostPage from './Pages/PostPage';
 import { ThemeContext } from './ThemeContext';
 
 function App() {
 
   const {theme} = useContext(ThemeContext);
   return (
+
+    <BrowserRouter>
     <div className={`container ${theme}`}>
-      <Navbar />
-      <div className="main">
-        <h1>Posts</h1>
-        <ul>
-          <li>
-            <h2>Post 1</h2>
-            <p>post 1 content</p>
-          </li>
-          <li>
-            <h2>Post 2</h2>
-            <p>post 2 content</p>
-          </li>
-        </ul>
-      </div>
-      <div className="footer">Awesome blog. All rights reserved</div>
+    <Navbar />
+    <div className="main">
+    <Routes>
+    <Route path='/post/:postId' element={<PostPage />} />
+    <Route path='/' element={<HomePage />} />
+    
+    </Routes>
+      
     </div>
+    <div className="footer">Awesome blog. All rights reserved</div>
+  </div>
+
+    </BrowserRouter>
+    
   )
 }
 
