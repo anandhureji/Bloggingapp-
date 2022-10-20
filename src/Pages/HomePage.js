80560d8172f9bf70538b0249c49a33cca1dd5b09
Reji <2111896@cognizant.com>
2022-10-20 12:55:56 +0530

    reducer hook feature added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,19 +1,72 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useReducer } from 'react'
+
+const reducer = (state,action) =>{
+    switch (action.type) {
+        case 'POSTS_REQUEST':
+            return {...state,loading:true};
+            break;
+        case 'POSTS_SUCCESS':
+            return {...state,loading:false,posts:action.payload,error:''};
+            break;
+        case 'POSTS_FAIL':
+            return {...state,error:action.payload,loading:false};
+            break
+    
+        default:
+            return state
+            break;
+    }
+}
 
 function HomePage() {
+    const [state,dispatch] = useReducer(reducer,{loading:false,error:'',posts:[]});
+    const{loading,posts,error} = state;
+
+    const loadPost= async () =>{
+        dispatch({type:'POSTS_REQUEST'});
+        try {
+            const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            dispatch({type:'POSTS_SUCCESS',payload:data});
+            
+        } catch (error) {
+
+            dispatch({type:'POSTS_FAIL',payload:error.message});
+            
+        }
+    }
+    useEffect(()=>{
+
+        loadPost();
+
+    },[])
+
+
   return (
-    <div>
+    <div className='blog'>
+    <div className='content'>
+
     <h1>Posts</h1>
-      <ul>
-        <li>
-          <h2>Post 1</h2>
-          <p>post 1 content</p>
-        </li>
-        <li>
-          <h2>Post 2</h2>
-          <p>post 2 content</p>
-        </li>
-      </ul>
+
+    {
+        loading?(<div>Loding...</div>):
+        error?(<div>Error:{error}</div>):
+        posts.length===0?(<div>No post Found</div>):
+        (
+            <ul>
+            {posts.map((post)=>(
+                <li key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                </li>
+            ))}
+            </ul>
+        )
+
+    }
+    
+    </div>
+   
       
     </div>
   )
